84fd68f65a3b27d5de356128720fb7023435b0ca
Waheed <126814060+WaheedArafa@users.noreply.github.com>
2024-04-01 15:43:05 +0200

    Delete cpp/atcoder directory

--- cpp/atcoder/A - AtCoder Contest.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    string s;
-    cin >> s;
-    std::transform(s.begin(), s.end(), s.begin(), ::toupper);
-    cout << "A" << s[0] << "C";
-}
