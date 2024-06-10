c8371c106acf77d24f54a69cbbb6f40649fff2db
Waheed <126814060+WaheedArafa@users.noreply.github.com>
2024-06-10 12:08:08 +0300

    Delete cpp/atcoder/ABC directory

--- cpp/atcoder/ABC/A - K-City.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-int n, m;
-cin >> n >> m;
-int y = (n - 1) * (m - 1);
-cout << y;
-}
--- cpp/atcoder/ABC/A - Last Letter.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-int n;
-string s;
-cin >> n;
-cin >> s;
-cout << s[n-1];
-}
